910d49a656bac212b07cd3e124e793d26ac85d31
Sujal Raj <sujalraj611@gmail.com>
2025-03-23 02:32:35 +0530

    home state and home event bloc added

--- lib/feature/home/bloc/home_event.dart
+++ lib/feature/home/bloc/home_event.dart
@@ -2,3 +2,21 @@ part of 'home_bloc.dart';
 
 @immutable
 sealed class HomeEvent {}
+
+class HomeInitialEvent extends HomeEvent {}
+
+class HomeProductWishlistButtonClickedEvent extends HomeEvent {
+  final Product clickedProduct;
+
+  HomeProductWishlistButtonClickedEvent({required this.clickedProduct});
+}
+
+class HomeProductCartButtonClickedEvent extends HomeEvent {
+  final Product clickedProduct;
+
+  HomeProductCartButtonClickedEvent({required this.clickedProduct});
+}
+
+class HomeWishListButtonNavigationEvent extends HomeEvent {}
+
+class HomeCartButtonNavigationEvent extends HomeEvent {}
--- lib/feature/home/bloc/home_state.dart
+++ lib/feature/home/bloc/home_state.dart
@@ -3,4 +3,24 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeState {}
 
+abstract class HomeActionState extends HomeState {}
+
 final class HomeInitial extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeLoadedSuccessState extends HomeState {
+  final List<Product> products;
+
+  HomeLoadedSuccessState({required this.products});
+}
+
+class HomeErrorState extends HomeState {}
+
+class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+class HomeNavigateToCartPageActionState extends HomeActionState {}
+
+class HomeProductItemWishlistedActionState extends HomeActionState {}
+
+class HomeProductItemCartedActionState extends HomeActionState {}
